90dfe47e7aea6b48e4486258c603b7e81c79a8bd
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-08-12 20:56:55 +0600

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,22 @@
+# Clone Lineage 21.0
+
+rm -rf .repo/local_manifests/
+
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+# Clone local_manifests repository
+
+git clone https://github.com/Rinto02/Local_Manifest.git --depth 1 -b lineage-21.0 .repo/local_manifests
+
+# repo sync
+/opt/crave/resync.sh
+
+# Set up build environment
+export BUILD_USERNAME=rinto
+export BUILD_HOSTNAME=crave
+
+source build/envsetup.sh
+# build
+lunch lineage_RMX2020-userdebug
+
+
+m bacon
