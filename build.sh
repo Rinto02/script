8e134d4c377e42363505482dc4bf7544a768815a
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-08-13 07:43:04 +0600

    Initial script for matrixx

--- build.sh
+++ build.sh
@@ -1,19 +1,26 @@
-# Clone Lineage 21.0
+# Clone ProjectMatrixx
 
-rm -rf .repo/local_manifests/
+rm -rf .repo/local_manifests
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+
 # Clone local_manifests repository
-
-git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b lineage-21.0 .repo/local_manifests
+git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b matrixx .repo/local_manifests
 
 # repo sync
 /opt/crave/resync.sh
 
+# remove and clone
+rm -rf frameworks/base
+git clone --depth=1 https://github.com/Rinto02/matrixx_frameworks_base frameworks/base
+rm -rf frameworks/av
+git clone --depth=1 https://github.com/Rinto02/matrixx_frameworks_av frameworks/av
+rm -rf frameworks/native
+git clone --depth=1 https://github.com/Rinto02/matrixx_frameworks_native frameworks/native
+
 # Set up build environment
+. build/envsetup.sh
+export TZ=Asia/Dhaka
 export BUILD_USERNAME=rinto
 export BUILD_HOSTNAME=crave
-
-source build/envsetup.sh
-# build
 brunch RMX2020 userdebug
