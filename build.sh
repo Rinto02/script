2a64bd001cbab763d9c05a6881cf83aeb6f3424b
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-08-14 19:41:36 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,18 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
 
 # Clone local_manifests repository
-git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b matrixx .repo/local_manifests
+git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b infinity .repo/local_manifests
 
 # repo sync
 /opt/crave/resync.sh
-
-# remove and clone
-rm -rf frameworks/av
-git clone --depth=1 https://github.com/Rinto02/matrixx_frameworks_av frameworks/av
 
 # Set up build environment
 . build/envsetup.sh
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=rinto
 export BUILD_HOSTNAME=crave
-brunch RMX2020 userdebug
+lunch infinity_RMX2020-userdebug
+mka bacon
