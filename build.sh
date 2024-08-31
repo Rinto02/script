82a596daa11785268c156a2a8e56fbe4b66950c2
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-09-01 02:38:16 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,36 @@
-# Clone Lineage 21.0
-
 rm -rf .repo/local_manifests/
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-# Clone local_manifests repository
+# repo init rom
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
 
-git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b lineage-21.0 .repo/local_manifests
+echo ==================
+echo Repo init success
+echo ================== 
 
-# repo sync
+# Local manifests
+git clone https://github.com/Gtajisan/local_manifests -b afterlife .repo/local_manifests
+
+echo ============================ 
+echo Local manifest clone success
+echo ============================
+
+# build
 /opt/crave/resync.sh
 
-# Set up build environment
-export BUILD_USERNAME=rinto
-export BUILD_HOSTNAME=crave
+echo =============
+echo Sync success
+echo =============
 
+# Export
+export BUILD_USERNAME=Infiniteluck777
+export BUILD_HOSTNAME=crave echo
+
+======= Export Done ======
+# Set up build environment
 source build/envsetup.sh
-# build
-brunch RMX2020 userdebug
+echo ====== Envsetup Done ======= 
+
+# lunch 
+. build/envsetup.sh
+goafterlife Mi439_4_19
+goafterlife Mi439_4_19 userdebug
