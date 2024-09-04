0fd500899e9128aa31e57cef60c7290cf978435f
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-09-04 06:39:25 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --depth=1 -u https://github.com/ProjectEverest/manifest -b 14 --git-lfs --depth=1
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
 
 echo ==================
 echo Repo init success
 echo ==================
 
 # Local manifests
-git clone https://github.com/Rinto02/Local-Manifest --depth 1 -b everest .repo/local_manifests
+git clone https://github.com/Rinto02/Local-Manifest --depth 1 -b rising .repo/local_manifests
 echo ============================ 
 echo Local manifest clone success
 echo ============================
@@ -36,7 +36,7 @@ export BUILD_USERNAME=Rinto02
 export BUILD_HOSTNAME=crave
 export TARGET_RELEASE=ap2a
 export TARGET_PRODUCT=lineage_RMX2020
-export TARGET_BUILD_VARIANT=eng
+export TARGET_BUILD_VARIANT=user
 
 echo ======= Export Done ======
 
@@ -46,9 +46,5 @@ echo ====== Envsetup Done =======
 
 # lunch 
 . build/envsetup.sh
-breakfast RMX2020 eng
-lunch lineage_RMX2020-ap2a
-lunch lineage_RMX2020-eng
-lunch lineage_RMX2020-ap2a-eng
-make installclean
-mka everest
+riseup RMX2020 user
+mka bacon
